7215f2ba2dac66aeeb7d94cce42c78c29e2fe64b
BelovDmitry <1@belov.es>
2024-04-04 22:12:29 +0300

    Initial comit

---
+++ Rick-and-Morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick-and-Morty
+//
+//  Created by Dmitry Belov on 04.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+    }
+
+
+}
+
